3db21bf9d956fdbaa0491e06b64b903cceaedb40
AusmitM <ausmitm715@gmail.com>
2025-10-02 18:33:04 -0500

    updated comments

--- Metrics.py
+++ Metrics.py
@@ -10,16 +10,19 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 sbert_model = SentenceTransformer('all-MiniLM-L6-v2')
 
+# COSINE SIMILARITY BETWEEN TWO TEXTS USING TF-IDF VECTORIZER
 def CosineSimilarity(text1, text2):
     vec = TfidfVectorizer().fit_transform([text1, text2])
     return float(cosine_similarity(vec[0], vec[1])[0][0])
 
+# SEMANTIC SIMILARITY BETWEEN TWO TEXTS USING PRETRAINED BERT MODEL
 def WebBertSim(text1, text2):
     emb1 = sbert_model.encode(text1, convert_to_tensor=True)
     emb2 = sbert_model.encode(text2, convert_to_tensor=True)
     cosine_score = float(util.pytorch_cos_sim(emb1, emb2))
     return 1 + ((cosine_score + 1) * 4 / 2)  # scale [-1,1] → [1,5]
 
+# SIMILARITY BETWEEN TWO TEXTS USING GPT-4
 def GPT4AllSim(text1, text2):
     response = client.chat.completions.create(
     model="gpt-4.1",
@@ -34,8 +37,8 @@ def GPT4AllSim(text1, text2):
         return 1.0
     else:
         return 0.0
-    # print(response.choices[0].message.content)
 
+# SUCCESS RATE CALCULATION
 def SuccessRate(successes, total):
     if total == 0:
         return 0.0
